71cbc29e870a371da5fd3f10085259196a0bc734
Mario <150532526+InerteoO@users.noreply.github.com>
2024-12-14 14:06:21 +0100

    Add files via upload

---
+++ core.lua
@@ -0,0 +1,5 @@
+-- Addon pour simplifier la vie du FP
+FPAssist = {}
+
+FPAssist.config = {}
+
---
+++ events.lua
@@ -0,0 +1,8 @@
+
+
+local frame = CreateFrame("Frame")
+frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+
+frame:SetScript("OnEvent", function(self, event)
+        DEFAULT_CHAT_FRAME:AddMessage("FPAssist chargé")
+end)
